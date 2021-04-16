1eb7339464452bd051e5d5cad8e7aaabb29f3c4c
Søren Jacob Lauritsen <m@sjl.dk>
2021-04-16 14:56:23 +0200

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from "firebase-functions";
 
 export const waitTimes = functions.https.onRequest((request, response) => {
-  response.send({"number": 210});
+  response.send({"number": 450});
 });
